cfcd6d6b1c9aad6b0ee4adbfcf59289cb141e0e1
dsalvat596 <dsalvat596@gmail.com>
2018-05-16 15:27:50 +0300

    added shuffle

--- public/eventHandler.js
+++ public/eventHandler.js
@@ -12,6 +12,8 @@ class EventsHandler {
              .then(()=>{
                  this.cardRepository.dupCards()
                 console.log(this.cardRepository.dbCards)
+                this.cardRepository.shuffleCards(this.cardRepository.cardsCollection);
+                console.log(this.cardRepository.cardsCollection);
                 this.gameRenderer.renderGameBoard(this.cardRepository.cardsCollection);
             })
         })
