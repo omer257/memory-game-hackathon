ed26bd363f9706ee717adf90f9e95542541f07f7
nosteiner <37960334+nosteiner@users.noreply.github.com>
2018-05-17 14:36:12 +0300

    update

--- public/matchChecker.js
+++ public/matchChecker.js
@@ -3,7 +3,16 @@ class MatchChecker {
         this.openedCards = [];
     }
 
-   
+    checkMach(name) {
+        this.openedCards.push(name);
+        if (this.openedCards.length === 2) {
+            if (this.openedCards[0] === this.openedCards[1]) {
+                return 2;
+            } else {
+                return 0;
+            }
+        } return 1;
+    }
 }
 
 export default MatchChecker;
