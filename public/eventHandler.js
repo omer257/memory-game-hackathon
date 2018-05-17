9dc1ad7ff4ac257fbbf79ec8d6f4e80c359007c5
nosteiner <37960334+nosteiner@users.noreply.github.com>
2018-05-17 15:34:31 +0300

    ///

--- public/eventHandler.js
+++ public/eventHandler.js
@@ -10,59 +10,55 @@ class EventsHandler {
         $('#getCards').on('click', () => {
             this.cardRepository.getCards()
                 .then(() => {
-
-            this.cardRepository.shuffleCards(this.cardRepository.cardsCollection);
-            this.gameRenderer.renderGameBoard(this.cardRepository.cardsCollection);
-                })
-        })
-
-                   
                     this.cardRepository.cardsCollection = this.cardRepository.shuffleCards(this.cardRepository.cardsCollection);
-                    console.log(this.cardRepository.cardsCollection);
                     this.gameRenderer.renderGameBoard(this.cardRepository.cardsCollection);
                 })
-        })
-        var clickCount = 0;
-
-    }
-
-    clickCard() {
-
-        $(document).on('click', '.card__face', (event) => {
-            let cardId = $(event.currentTarget).id;
-            let cardName = $(event.currentTarget).name;
-
-            this.cardRepository.changeState(cardId);
-            this.gameRenderer.renderGameBoard(this.cardRepository.cardsCollection);
-            
            
-            // var matchValues = this.matchChecker.cardOpen(cardId, cardName);
-
-            // if (matchValues[3] === true) {
-            //     numberOfMatches += 1;
-
-            //     if (numberOfMatches === 4) {
-            //         this.gameRenderer.gameOver();
-            //         return;
-            //     }
-            // } else {
-            //     this.cardRepository.updatedCards(matchValues[0], matchValues[1])
-            // }
-
-            //this.gameRenderer.renderGameBoard(updatedcardsCollection)
-        })
-
-
-
-        $('.cards-container').on('click','.state-{{state}}', () => {
-            
+            console.log(this.cardRepository.cardsCollection);
+            this.gameRenderer.renderGameBoard(this.cardRepository.cardsCollection);
         })
     }
 
-    incrementClickCounter(){    
-        clickCount++;    
 
-    }
+
+
+clickCard() {
+
+    $(document).on('click', '.card__face', (event) => {
+        let cardId = $(event.currentTarget).id;
+        let cardName = $(event.currentTarget).name;
+
+        this.cardRepository.changeState(cardId);
+        this.gameRenderer.renderGameBoard(this.cardRepository.cardsCollection);
+
+
+        // var matchValues = this.matchChecker.cardOpen(cardId, cardName);
+
+        // if (matchValues[3] === true) {
+        //     numberOfMatches += 1;
+
+        //     if (numberOfMatches === 4) {
+        //         this.gameRenderer.gameOver();
+        //         return;
+        //     }
+        // } else {
+        //     this.cardRepository.updatedCards(matchValues[0], matchValues[1])
+        // }
+
+        //this.gameRenderer.renderGameBoard(updatedcardsCollection)
+    })
+
+
+
+    $('.cards-container').on('click', '.state-{{state}}', () => {
+
+    })
+}
+
+incrementClickCounter(){
+    clickCount++;
+
+}
 
 }
 
